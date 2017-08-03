3762be6d149f26608c9ba99b089e1a157399688a
AxcLogo <532432339@qq.com>
2017-08-03 10:12:01 +0800

    新增API，图片旋转

--- AxcUIKit/AxcUIKit/AxcUIKit.h
+++ AxcUIKit/AxcUIKit/AxcUIKit.h
@@ -26,10 +26,10 @@
 #import "UIImage+AxcImageName.h"              // 为Image扩展根据图片大小来是否加入内存缓存
 #import "UIImage+AxcQRCode.h"                 // Image扩展自动生成二维码
 #import "UIImage+AxcSpecialEffectsDrawing.h"  // 图像算法/形态学图像处理
+#import "UIImage+AxcImageRotating.h"          // 图像旋转
 
 // MARK: UIImageView+
 #import "UIImageView+AxcNetworkLoadImage.h"   // 根据AFN和SD结合简单封装的一套异步加载图片的小型框架
-#import "Axc_WebimageCache.h"                 // 清理图片缓存的工具
 #import "UIImageView+AxcImageLoader.h"        // ImageView自动添加Progress指示器扩展
 #import "UIImageView+AxcWebCache.h"           // ImageView的图片处理
 #import "UIImageView+AxcFilter.h"             // 快速滤镜扩展
@@ -52,7 +52,7 @@
 #import "UIViewController+AxcSemiModal.h"     // VC推出动画效果框架扩展
 
 
-
+#pragma mark - 工具类
 /***********************************工具类***************************************/
 // MARK: NSString+
 #import "NSString+AxcTextCalculation.h"       // 工具类，计算文字宽和高
@@ -71,6 +71,8 @@
 #import "UITableViewCell+AxcAnimation.h"      // UITableViewCell预设动画
 // MARK: UIViewController+
 #import "UIViewController+AxcVCBackButtonEvent.h" //获取返回按钮的点击事件
+
+#import "Axc_WebimageCache.h"                 // 清理图片缓存的工具
 
 
 
