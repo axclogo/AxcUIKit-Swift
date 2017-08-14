0f4aa2f69fea70fe2a5ad1d5baa39a458ca3a717
AxcLogo <532432339@qq.com>
2017-08-14 17:06:51 +0800

    预拓展一下基类函数，未更新任何控件

--- AxcUIKit/BaseVC/SampleBaseVC.m
+++ AxcUIKit/BaseVC/SampleBaseVC.m
@@ -25,6 +25,31 @@
     self.automaticallyAdjustsScrollViewInsets = NO;
     self.instructionsLabelHeight = 100;
     
+    
+    // 设置Bar半透明属性
+    //    self.navigationController.navigationBar.translucent               = YES;
+    
+    // 设置开启Push后右滑返回手势
+    //    self.navigationController.interactivePopGestureRecognizer.enabled = YES;
+    
+    // 设置取消ScrollerView自适应属性
+    //    self.automaticallyAdjustsScrollViewInsets                         = NO;
+    
+    // 设置默认滑动时不隐藏NavBar导航条
+    //    self.navigationController.hidesBarsOnSwipe                        = NO;
+    
+    // 设置子类的NavBar上的渲染颜色
+    //    self.navigationController.navigationBar.tintColor         = [UIColor whiteColor];
+    
+    // 设置NavBar底层View的颜色
+    //    self.navigationController.navigationBar.backgroundColor   = [UIColor clearColor];
+    
+    // 设置NavBar顶层View的颜色
+    //    self.navigationController.navigationBar.barTintColor      = [UIColor clearColor];
+    
+    // 设置子类的Tabbar风格
+    //    self.navigationController.navigationBar.barStyle          = UIBarStyleBlack;
+    
 }
 - (void)AxcBase_addRightBarButtonItems:(NSArray *)items{
     NSMutableArray *arr_M = [NSMutableArray array];
@@ -48,6 +73,47 @@
                                               action:@selector(AxcBase_clickRightBtn:)];
 }
 - (void)AxcBase_clickRightBtn:(UIBarButtonItem *)sender{}
+
+
+// MARK:Alent函数
+// 弹出一个Alent
+- (void)AxcBase_PopAlertViewWithTitle:(NSString *)title
+                              Message:(NSString *)message
+                              Actions:(NSArray <NSString *>*)actions
+                              handler:(void (^ __nullable)(UIAlertAction *action))handler{
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:title
+                                                                             message:message
+                                                                      preferredStyle:UIAlertControllerStyleAlert];
+    [actions enumerateObjectsUsingBlock:^(NSString * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        UIAlertAction *action = [UIAlertAction actionWithTitle:obj
+                                                         style:UIAlertActionStyleDefault
+                                                       handler:^(UIAlertAction *action) {
+                                                           handler(action);
+                                                       }];
+        [alertController addAction:action];
+    }];
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+// 弹出一个警告框
+- (void)AxcBase_PopAlertWarningMessage:(NSString *)message{
+    [self AxcBase_PopAlertViewWithTitle:@"警告"
+                                Message:message
+                                Actions:@[@"确定"]
+                                handler:nil];
+}
+// 弹出一个提示框
+- (void)AxcBase_PopAlertPromptMessage:(NSString *)message
+                            OKHandler:(void (^ __nullable)(UIAlertAction *action))OKHandler
+                        cancelHandler:(void (^ __nullable)(UIAlertAction *action))cancelHandler{
+    [self AxcBase_PopAlertViewWithTitle:@"提示"
+                                Message:message
+                                Actions:@[@"确定",@"取消"]
+                                handler:^(UIAlertAction * _Nonnull action) {
+                                    if ([action.title isEqualToString:@"确定"]) {
+                                        OKHandler(action);
+                                    }else cancelHandler(action);
+                                }];
+}
 
 
 - (UILabel *)instructionsLabel{
