27654501b85257b3777345358fc02e98a9dd5b5d
AxcLogo <532432339@qq.com>
2017-09-05 15:35:58 +0800

    增加多Item布局基类

---
+++ AxcUIKit/AxcUIKit/Axc_Controls/AxcUI_CollectionLayout/AxcUI_MultipleBaseLayout.m
@@ -0,0 +1,13 @@
+//
+//  AxcUI_MultipleBaseLayout.m
+//  AxcUIKit
+//
+//  Created by Axc on 2017/9/5.
+//  Copyright © 2017年 Axc_5324. All rights reserved.
+//
+
+#import "AxcUI_MultipleBaseLayout.h"
+
+@implementation AxcUI_MultipleBaseLayout
+
+@end
