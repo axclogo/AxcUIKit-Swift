4ff08a31041ed5a4ee41100172e64d7c21dbe464
AxcLogo <532432339@qq.com>
2017-09-05 15:39:41 +0800

    修改布局部分的目录结构，其他不做更改

---
+++ AxcUIKit/AxcUIKit/Axc_Controls/AxcUI_CollectionLayout/AxcUI_SingleBaseLayout/AxcUI_SingleBaseLayout.m
@@ -0,0 +1,13 @@
+//
+//  AxcUI_SingleBaseLayout.m
+//  AxcUIKit
+//
+//  Created by Axc on 2017/9/5.
+//  Copyright © 2017年 Axc_5324. All rights reserved.
+//
+
+#import "AxcUI_SingleBaseLayout.h"
+
+@implementation AxcUI_SingleBaseLayout
+
+@end
